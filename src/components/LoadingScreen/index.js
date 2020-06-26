9525e0b1433d30fd1f4a4518b543f1455aae072c
SerraZ3 <serra.henrique3@gmail.com>
2020-06-26 16:28:08 -0300

    :wolf: Created loading screen

---
+++ src/components/LoadingScreen/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Grid, Backdrop, CircularProgress} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+    backgroundColor: '#ffffff99',
+  },
+}));
+function LoadingScreen({open}) {
+  const classes = useStyles();
+  return (
+    <Backdrop className={classes.backdrop} open={open}>
+      <CircularProgress
+        size={80}
+        style={{color: process.env.REACT_APP_PRIMARY_COLOR}}
+      />
+    </Backdrop>
+  );
+}
+
+export default LoadingScreen;
