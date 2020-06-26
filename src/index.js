e774d7ba28bdc5f465940ae23f6217cf2d459569
SerraZ3 <serra.henrique3@gmail.com>
2020-06-26 16:37:17 -0300

    :wolf: Added provider and persistGate for redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
+import './styles.css';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
