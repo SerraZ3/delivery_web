2b960b66dd4fdfbb029ef929876f8e5c5d390dc6
SerraZ3 <serra.henrique3@gmail.com>
2020-06-28 05:57:39 -0300

    :wolf: Clear code

--- src/components/LoadingScreen/index.js
+++ src/components/LoadingScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Backdrop, CircularProgress} from '@material-ui/core';
+import {Backdrop, CircularProgress} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
