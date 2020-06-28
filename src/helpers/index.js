c784e015e531de8707ae754236f0a85045b826e7
SerraZ3 <serra.henrique3@gmail.com>
2020-06-28 05:58:27 -0300

    :wolf: Added formatMoney, getPaymentType and convertDatebirthday

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -24,3 +24,37 @@ export const convertDate = (date) => {
     hour + ':' + minutes + ':' + seconds + ' ' + day + '/' + month + '/' + year
   );
 };
+export const convertDateBirthDay = (date) => {
+  function checkZero(data) {
+    if (data.length === 1) {
+      data = '0' + data;
+    }
+    return data;
+  }
+  let newDate = new Date(date);
+  let day = newDate.getDate() + '';
+  let month = newDate.getMonth() + 1 + '';
+  let year = newDate.getFullYear() + '';
+
+  day = checkZero(day);
+  month = checkZero(month);
+  year = checkZero(year);
+
+  return day + '/' + month + '/' + year;
+};
+
+export const formatMoney = (money) =>
+  parseFloat(money).toFixed(2).toString().replace('.', ',');
+
+export const getPaymentType = (data) => {
+  switch (data) {
+    case 'cash':
+      return 'Dinheiro';
+    case 'credit_card':
+      return 'Cartão de crédito';
+    case 'debit_card':
+      return 'Cartão de débito';
+    default:
+      return 'Error';
+  }
+};
